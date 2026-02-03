03229af90a3b8facad5e9e9e64650de1fd7a8b92
khkk24 <franciscokn3@gmail.com>
2026-02-03 18:35:17 -0300

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
-    domains: ['avatars.githubusercontent.com', 'github.com'],
-  },
-  i18n: {
-    locales: ['pt', 'fr', 'en'],
-    defaultLocale: 'pt',
+    unoptimized: true,
   },
 }
 
